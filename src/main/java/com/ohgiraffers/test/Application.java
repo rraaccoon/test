3ca400e1f801157ba5b86d9b4c2ef0995ebce227
pyunghun <raccoon.ohgiraffers@gmail.com>
2024-08-23 13:10:30 +0900

    [깃허브 테스트]

---
+++ src/main/java/com/ohgiraffers/test/Application.java
@@ -0,0 +1,26 @@
+package com.ohgiraffers.test;
+
+public class Application {
+
+    // 한 줄 주석
+    // 필기를 해보아요
+
+    // 여러 줄 주석
+    /*여러
+    * 줄
+    * 주석
+    **/
+    /* 수업목표.  */
+    /* fixme.  */
+
+    /* 필기.
+    *   main 영역은 프로그램을 동작시킬 수 있는 영역이다.
+    *  */
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("안녕하세요");
+//        System.out.println(안녕하세요);
+    }
+
+}
